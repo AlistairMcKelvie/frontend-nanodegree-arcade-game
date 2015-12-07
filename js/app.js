a1784d8de898bbd29ac4b72d9d840fb89fcff69f
Alistair McKelvie <alistair.mckelvie@gmail.com>
2015-12-07 17:54:17 +1100

    feat: Add moving enemies which loop

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(initY, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.y = initY;
+    this.x = 0;
+    this.speed = speed;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,6 +17,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += dt * this.speed;
+    console.log(ctx);
+    console.log(ctx.canvas.width);
+    if (this.x > ctx.canvas.width) {
+        this.x = this.x % ctx.canvas.width;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -41,7 +50,7 @@ Player.prototype.update = function() {
 // Place the player object in a variable called player
 //
 // TODO: add enemies to all enemies
-var allEnemies = [new Enemy];
+var allEnemies = [new Enemy(60, 100)];
 var player = new Player;
 
 
