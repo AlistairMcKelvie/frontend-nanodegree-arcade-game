b5b68547c757bbe0680b36e08f92e600dae1152e
Alistair McKelvie <alistair.mckelvie@gmail.com>
2015-12-07 17:31:12 +1100

    feat: Add empty Player class and enemies array

--- js/app.js
+++ js/app.js
@@ -24,12 +24,25 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    //TODO: add properties
+};
 
+Player.prototype.render = function() {
+    //TODO: impement this
+};
+
+Player.prototype.update = function() {
+    //TODO: implement this
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+//
+// TODO: add enemies to all enemies
+var allEnemies = [new Enemy];
+var player = new Player;
 
 
 // This listens for key presses and sends the keys to your
